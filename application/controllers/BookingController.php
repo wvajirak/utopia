d2c223f13892a91246a9c38ca6cc042e576de259
Vajira Kuruppuarachchi <arijav@gmail.com>
2014-02-14 02:32:18 +0000

    Complete the booking usercase

--- application/controllers/BookingController.php
+++ application/controllers/BookingController.php
@@ -10,13 +10,38 @@ class BookingController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+        $select = Application_Service_QueryHelper::availabilityQuery();
+        $this->view->rows = $select->query()->fetchAll();
     }
 
+    public function bookAction()
+    {
+        $active_tour_id = (int) $this->getParam('id',0);
+        
+//        $this->view->active_tour_id  = $active_tour_id;
+        
+        $form = new Application_Form_Booking();
+        $form ->getElement('active_tour_id')->setValue($active_tour_id);
+        
+        if ($this->getRequest ()->isPost ()) {    	
+            $formData = $this->getRequest ()->getPost ();
+           // die(print_r($formData));
+            if ($form->isValid($formData)) {
+            
+                Application_Service_QueryHelper::save($form);
+          
+            } else {
+                
+                die("Form is invalid");
+                
+            }
+                                
+            $this->redirect("/booking/index");  
+        }
+        
+         $this->view->form = $form;
 
-
+    }
+    
 
 }
-
-
-
