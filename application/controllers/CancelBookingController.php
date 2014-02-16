88decaf1e2d46569eab2ed7e8dcbe76da46b3bc2
Vajira Kuruppuarachchi <arijav@gmail.com>
2014-02-16 12:44:11 +0000

    complete canel booking feature

--- application/controllers/CancelBookingController.php
+++ application/controllers/CancelBookingController.php
@@ -16,7 +16,21 @@ class CancelBookingController extends Zend_Controller_Action
 
     public function cancelAction()
     {
-        // action body
+        $booking_id = (int) $this->getParam('id',0);
+        $booking_tb = new Application_Model_DbTable_Bookings();
+        $row = $booking_tb->find($booking_id);
+        if($row){
+            $customer_id = $row['customer_id'];
+           // die("$booking_id");
+            $booking_tb->deleteBooking($booking_id);
+            $customer_tb = new Application_Model_DbTable_Customers();
+            $customer_tb->deleteCustomer($customer_id);
+            
+        }else{
+            throw new Exception("Could not find row $booking_id");
+        }
+        
+        $this->redirect("/cancel-booking/index"); 
     }
 
 
